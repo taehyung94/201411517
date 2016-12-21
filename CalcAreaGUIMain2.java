f888ca503430b4a347c3a38724bc423b97cc6e1a
김태형_201411517 <rlaxogud94@naver.com>
2016-12-21 23:21:32 +0900

    Update CalcAreaGUIMain2.java

--- CalcAreaGUIMain2.java
+++ CalcAreaGUIMain2.java
@@ -1,12 +1,17 @@
 package com.sd.gui;
 
-import javax.swing.*
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 
 public class CalcAreaGUIMain2 extends JFrame implements ActionListener{
 	JTextField radiusTf1;
-	public CalcAreaGUIMain(){
-	Jpanel panel=new JPanel(new GridLayout(3,2));
+	JTextField AreaTf2;
+	JButton b1;
+	JButton b2;
+	public CalcAreaGUIMain2(){
+	JPanel panel=new JPanel(new GridLayout(3,2));
 	getContentPane().add(panel);
 	JLabel label1=new JLabel("반지름");
 	JLabel label2=new JLabel("넓이");
@@ -14,7 +19,8 @@ public class CalcAreaGUIMain2 extends JFrame implements ActionListener{
 	JTextField AreaTf2=new JTextField(10);
 	JButton b1=new JButton("계산");
 	JButton b2=new JButton("취소");
-	b1.addActionListener();
+	b1.addActionListener(this);
+	b2.addActionListener(this);
 	panel.add(label1);
 	panel.add(label2);
 	panel.add(radiusTf1);
@@ -25,15 +31,19 @@ public class CalcAreaGUIMain2 extends JFrame implements ActionListener{
 	setVisible(true);
 	}
 	public void actionPerformed(ActionEvent e){
-		Jbutton b=(JButton)e.getSource();
-		b.setText("계산했습니다.");
-	    Double r=Double.ParseDouble(radiusTf1.getText());
-	    areaTf2.setText(Double.toString(Math.PI*Math.pow(r,2)));
+		if(e.getSource().equals(b1)){
+			Double r=Double.parseDouble(radiusTf1.getText());
+			AreaTf2.setText(Double.toString(Math.PI*Math.pow(r,2)));
+		}
+		else{
+			radiusTf1.setText("");
+			AreaTf2.setText("");		
+		}
 	}
-	public static void main(){
+	public static void main(String args[]){
 		CalcAreaGUIMain2 c=new CalcAreaGUIMain2();
 		c.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		c.pack();
-		c.setVisible();
+		c.setVisible(true);
 	}
 }
