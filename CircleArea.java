cf545b99f989b8fa58d51ab5e5df6b1eafb629ef
김태형_201411517 <rlaxogud94@naver.com>
2016-12-22 00:27:07 +0900

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class CircleArea{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		Double r=sc.nextDouble();
+		System.out.printf("%.1f",r*r*Math.PI);
+	}
+}
