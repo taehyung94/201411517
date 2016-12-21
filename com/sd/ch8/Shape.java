922efb6143a3e52466f11cd792021a46b5a4fe89
김태형_201411517 <rlaxogud94@naver.com>
2016-12-21 22:41:04 +0900

    Add files via upload

---
+++ com/sd/ch8/Shape.java
@@ -0,0 +1,11 @@
+public abstract class Shape{
+	public String name="Shape";
+	public abstract double calcArea();
+	public abstract double calcPerimeter();
+	public String getName(){
+		return this.name;
+	}
+	public String toString(){
+		return "Shape..."+super.toString();
+	}
+}
