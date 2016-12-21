e2e205a4dc650d95e9fcafbe241335ecb55d4b74
김태형_201411517 <rlaxogud94@naver.com>
2016-12-22 00:58:08 +0900

    Add files via upload

---
+++ ReadFile.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class ReadFile{
+	public static void main(String[] args){
+		Scanner name=new Scanner(new File("test.txt");
+		String line=name.nextLine();
+		System.out.println(line);
+	}
+}
